60204a37add7bedf731d7482eae34be581654ec4
gamingflexer <osurve20ece@student.mes.ac.in>
2022-11-12 19:59:50 +0530

    added Kaggle account

--- react-portfolio-template-main/components/Footer/index.js
+++ react-portfolio-template-main/components/Footer/index.js
@@ -3,7 +3,7 @@ import Socials from "../Socials";
 import Link from "next/link";
 import Button from "../Button";
 
-const Footer = ({}) => {
+const Footer = ({ }) => {
   return (
     <>
       <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
@@ -26,7 +26,7 @@ const Footer = ({}) => {
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
         <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
+          <a className="underline underline-offset-1">Om Surve</a>
         </Link>
       </h1>
     </>
